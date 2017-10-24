a8f41553f93c37cf1353de550be990aac084460a
Davide <mussaridavide@gmail.com>
2017-10-24 20:09:53 +0200

    Dewey - centinaia

--- src/app/catalogo-argomento/catalogo-argomento.component.ts
+++ src/app/catalogo-argomento/catalogo-argomento.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {Http, Response, RequestOptions, Headers} from '@angular/http';
+import { GetJsonService } from '../get-json.service';
 
 @Component({
-  selector: 'app-catalogo-argomento',
-  templateUrl: './catalogo-argomento.component.html',
-  styleUrls: ['./catalogo-argomento.component.scss']
+	selector: 'app-catalogo-argomento',
+	templateUrl: './catalogo-argomento.component.html',
+	styleUrls: ['./catalogo-argomento.component.scss'],
+	providers: [GetJsonService]
 })
 export class CatalogoArgomentoComponent implements OnInit {
 
-  constructor() { }
+	dewey: any;
+	paramCentinaia: any;
 
-  ngOnInit() {
-  }
+	constructor(private downloadJson: GetJsonService, private paramsRoute: ActivatedRoute) {
+	}
+
+	ngOnInit() {
+		this.downloadJson.getData("../json/dewej.json").subscribe((data) => {
+			this.dewey = data;
+			console.log(this.dewey);
+		});
+		
+		this.paramsRoute.params.subscribe(params => {
+       		this.paramCentinaia = params['centinaia'];
+		});
+		
+	}
 
 }
--- src/app/get-json.service.ts
+++ src/app/get-json.service.ts
@@ -8,7 +8,7 @@ export class GetJsonService {
   constructor(public http:Http) { }
 
 getData(str: string) {
-    return this.http.get(str).map((res:Response) => res.json().YOUR_JSON_HEADER);
+    return this.http.get(str).map((res:Response) => res.json());
   }
 
 }

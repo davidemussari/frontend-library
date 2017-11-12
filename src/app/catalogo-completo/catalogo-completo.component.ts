5220d4bd5f7aa1fb2fe46a1c98f2db0fb4e284c6
Davide <mussaridavide@gmail.com>
2017-11-12 23:07:33 +0100

    Ordinamento e filtro sulla colonna argomento

--- src/app/catalogo-completo/catalogo-completo.component.ts
+++ src/app/catalogo-completo/catalogo-completo.component.ts
@@ -2,6 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GetJsonService } from '../get-json.service';
 
+import {Comparator} from "clarity-angular";
+import {StringFilter} from "clarity-angular";
+
+class Element{
+    codiceArgomento: string;
+}
+
+
+class CodiceArgomentoSort implements Comparator<Element> {
+    aNumber : number = 0;
+    bNumber : number = 0;
+    compare(a: Element, b: Element) {
+        this.aNumber = +a.codiceArgomento;
+        this.bNumber = +b.codiceArgomento;
+        return this.aNumber - this.bNumber;
+    }
+}
+
+
+class CodiceArgomentoFiltro implements StringFilter<Element> {
+    accepts(element: Element, search: string):boolean {
+        return "" + element.codiceArgomento == search
+            || element.codiceArgomento.toString().toLowerCase().indexOf(search) >= 0;
+    }
+}
+
+
+
 @Component({
   selector: 'app-catalogo-completo',
   templateUrl: './catalogo-completo.component.html',
@@ -10,6 +38,8 @@ import { GetJsonService } from '../get-json.service';
 export class CatalogoCompletoComponent implements OnInit {
 
     catalogo: any;
+    private codiceArgomentoSort = new CodiceArgomentoSort();
+    private codiceArgomentoFiltro = new CodiceArgomentoFiltro();
 
   constructor(private router: Router, private downloadJson: GetJsonService) {
 
